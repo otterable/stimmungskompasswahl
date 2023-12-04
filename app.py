ba3e572c73f7097ebf5167a65fdc207c8f49e786
otterable <110373485+otterable@users.noreply.github.com>
2023-12-04 08:59:14 +0100

    a1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, session
+from flask import Flask, render_template, url_for, request, redirect, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User, Project, Vote, Comment
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
@@ -8,8 +8,24 @@ from forms import RegistrationForm, LoginForm
 from random import randint
 from urllib.parse import quote, unquote
 from markupsafe import Markup
+from twilio.rest import Client
 import logging
 import os
+import random
+import string
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Twilio credentials
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+twilio_number = os.environ.get('TWILIO_PHONE_NUMBER')
+
+print("Twilio Account SID:", account_sid)
+print("Twilio Auth Token:", auth_token)
+print("Twilio Phone Number:", twilio_number)
+
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -40,6 +56,12 @@ def index():
     logging.debug("Projects: %s", projects)
     return render_template('index.html', projects=projects)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/init_projects')
 def init_projects():
@@ -67,69 +89,176 @@ def download_file(filename):
     return send_from_directory(directory, decoded_filename)
 
 
+def calculate_age(dob):
+    today = datetime.today()
+    return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+
+
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        # Create a new user instance
-        user = User(
-            username=form.username.data, 
-            phone_number=form.phone_number.data,
-            name=form.name.data, 
-            surname=form.surname.data,
-            dob=form.dob.data, 
-            address=form.address.data
-        )
-        user.set_password(form.password.data)
-        db.session.add(user)
+    if request.method == 'POST':
+        print(request.form)  # Debug: print form data
+        phone_number = request.form.get('phone_number')  # Use .get() for safer access
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        surname = request.form['surname']
+        street = request.form['street']
+        town = request.form['town']
+        plz = request.form['plz']
+        bundesland = request.form['bundesland']
+        ip_address = request.remote_addr  # Capture IP address from request
 
-        # Generate OTP and handle verification (implementation depends on your method of sending OTP)
+        # Check for existing user with the same phone number or email
+        existing_user = User.query.filter((User.phone_number == phone_number) | (User.email == email)).first()
+        if existing_user:
+            flash('An account with this phone number or email already exists.', 'danger')
+            return jsonify({'success': False, 'message': 'User already exists'}), 400
+
+        # Generate OTP and handle verification
         otp = randint(100000, 999999)
-        # Send OTP to user's phone number and handle verification
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body=f"Your OTP is: {otp}",
+            from_=twilio_number,
+            to=request.form.get('phone_number')
+        )
+        logging.debug(f"Twilio message SID: {message.sid}")
 
+        # Save the user data in session temporarily
+        session['user_data'] = {
+            'phone_number': phone_number,
+            'email': email,
+            'password': password,
+            'name': name,
+            'surname': surname,
+            'street': street,
+            'town': town,
+            'plz': plz,
+            'bundesland': bundesland,
+            'ip_address': ip_address,
+            'otp': otp
+        }
+
+        logging.debug("User registered, OTP sent for verification")
+        return jsonify({'success': True, 'message': 'OTP sent successfully'})
         return redirect(url_for('verify_otp'))
 
-    return render_template('register.html', form=form)
+    return render_template('register.html')
 
+
+def generate_otp_and_send(phone_number):
+    otp = randint(100000, 999999)
+    client = Client(account_sid, auth_token)
+    try:
+        message = client.messages.create(
+            body=f"Your OTP is: {otp}",
+            from_=twilio_number,
+            to=phone_number
+        )
+        logging.debug(f"OTP sent: {message.sid}")
+    except Exception as e:
+        logging.error(f"Error sending OTP: {e}")
+        return None
+    return otp
+
+@app.route('/request_otp', methods=['POST'])
+def request_otp():
+    phone_number = request.form.get('phone_number')
+    user = User.query.filter_by(phone_number=phone_number).first()
+    if user:
+        otp = generate_otp_and_send(phone_number)
+        if otp:
+            session['reset_otp'] = otp
+            session['phone_number'] = phone_number
+            logging.debug(f"OTP generated and session updated for phone number {phone_number}")
+            return jsonify(success=True, message="OTP sent to your phone.")
+        else:
+            return jsonify(success=False, message="Failed to send OTP.")
+    return jsonify(success=False, message="Phone number not found.")
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        otp_entered = request.form.get('otp')
+        new_password = request.form.get('new_password')
+        phone_number = session.get('phone_number')
+        if 'reset_otp' in session and session['reset_otp'] == int(otp_entered):
+            user = User.query.filter_by(phone_number=phone_number).first()
+            if user:
+                user.set_password(new_password)
+                db.session.commit()
+                logging.debug(f"Password reset for user with phone number {phone_number}")
+                flash('Your password has been reset successfully.', 'success')
+                return redirect(url_for('login'))
+            else:
+                flash('Invalid phone number.', 'danger')
+        else:
+            flash('Invalid OTP. Please try again.', 'danger')
+    return render_template('reset_password.html')
+    
 
 @app.route('/verify_otp', methods=['GET', 'POST'])
 def verify_otp():
     if request.method == 'POST':
-        otp_entered = request.form.get('otp')
-        if 'otp' in session and session['otp'] == int(otp_entered):
-            user_id = session.pop('user_id')
-            user = User.query.get(user_id)
+        user_otp = request.form['otp']
+        if 'user_data' in session and session['user_data']['otp'] == int(user_otp):
+            user_data = session.pop('user_data')
+            new_user = User(
+                phone_number=user_data['phone_number'],
+                email=user_data['email'],
+                password_hash=generate_password_hash(user_data['password']),
+                name=user_data['name'],
+                surname=user_data['surname'],
+                street=user_data['street'],
+                town=user_data['town'],
+                plz=user_data['plz'],
+                bundesland=user_data['bundesland'],
+                ip_address=user_data['ip_address']
+            )
+            db.session.add(new_user)
             db.session.commit()
-            flash('Account created successfully!', 'success')
-            return redirect(url_for('login'))
+
+            # Log in the user automatically
+            login_user(new_user)
+
+            return jsonify({'success': True, 'message': 'Account successfully created'})
         else:
-            flash('Invalid OTP. Please try again.', 'danger')
+            return jsonify({'success': False, 'message': 'Invalid OTP'}), 400
+
     return render_template('verify_otp.html')
+
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    logging.debug("Login route accessed")
     if request.method == 'POST':
         username_or_phone = request.form.get('username_or_phone')
         password = request.form.get('password')
 
-        # Check if user exists with given username or phone number
-        user = User.query.filter((User.username == username_or_phone) | 
-                                 (User.phone_number == username_or_phone)).first()
+             # Adjust the filter to match the fields in your User model
+        user = User.query.filter((User.phone_number == username_or_phone) | (User.email == username_or_phone)).first()
+
 
         if user and user.check_password(password):
-            # User exists and password matches, log the user in
             login_user(user)
-            return redirect(url_for('index'))
+            logging.debug("Login successful")
+            flash('Login successful!', 'success')
+            return jsonify(success=True)
         else:
-            # User does not exist or password does not match
-            flash('Invalid credentials. Please register if you do not have an account.', 'danger')
-            return redirect(url_for('register'))
+            logging.debug("Login failed")
+            flash('Login failed - invalid credentials.', 'danger')
+            return jsonify(success=False)
 
     return render_template('login.html')
 
--- models.py
+++ models.py
@@ -1,18 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
-    dob = db.Column(db.Date, nullable=False)
-    address = db.Column(db.String(200), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    town = db.Column(db.String(100), nullable=False)
+    plz = db.Column(db.String(10), nullable=False)
+    bundesland = db.Column(db.String(50), nullable=False)
+    ip_address = db.Column(db.String(100))  # Assuming you'll set this at registration
+    highlight_id = db.Column(db.Integer)  # Assuming this is an integer field
+    account_creation = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
